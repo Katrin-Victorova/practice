1ac68196f24ad941fcf61981b4193aabd0140164
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-08 13:35:19 +0300

    Модули

---
+++ Модули/Пример.js
@@ -0,0 +1,14 @@
+import printMyName from "./Конспект.js"
+import sum from "./Конспект.js"
+import {
+  one,
+  two
+} from "./Конспект.js"
+
+printMyName() // Katrin
+
+const result = sum(10, 2)
+console.log(result)
+
+console.log(one)
+console.log(two)
