7b834f91c608c85ca0df1b5b9bd5841518ee1735
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-12 19:55:09 +0300

    New 35

---
+++ Codewars/Задачa 35.js
@@ -0,0 +1,8 @@
+// Sentence Smash
+// Склеить массив
+// Приходит массив из слов и мы должны сделать предложение
+// const worlds = ["hello", "world", "this", "is", "great"]
+
+const smash = (worlds) => worlds.join(" ")
+
+console.log(smash(["hello", "world", "this", "is", "great"]))
