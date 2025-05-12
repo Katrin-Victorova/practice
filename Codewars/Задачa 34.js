e2875e67ed18e8d923f99f512ce69d195d47f341
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-12 19:55:01 +0300

    New 34

--- Codewars/Задачa 34.js
+++ Codewars/Задачa 34.js
@@ -1,3 +1,27 @@
 // Reverse every other world in the string
+const reverse = (str) => 
+  str
+     .trim()
+     .split(" ")
+     .map((el, i) => i % 2 
+     ? el
+       .split("")
+       .reverse()
+       .join("") 
+       : el )
+     .join(" ");
 
-function reverse(string) {}
+// function reverse(str) {
+//     return str
+//     .trim()
+//     .split(" ")
+//     .map((el, i) => {
+//         return i % 2
+//         ? el.split("").reverse().join("")
+//         : el;
+//         })
+//         .join(" ");
+// }
+
+
+console.log(reverse("I really dont like strings!"))
