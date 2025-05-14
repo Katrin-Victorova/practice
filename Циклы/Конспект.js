ab56a82fe181c5419fa8e502de083354b3bb11f9
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-14 09:52:01 +0300

    Циклы - метка

--- Циклы/Конспект.js
+++ Циклы/Конспект.js
@@ -198,3 +198,15 @@ console.log(result)
 // ****
 // *****
 // ******
+
+// метка
+first: for (let i = 0; i < 3; i++) {
+	console.log(`First level: ${i}`)
+	for (let j = 0; j < 3; j++) {
+		console.log(`Second level: ${j}`)
+		for (let k = 0; k < 5; k++) {
+			if (k === 2) break first //continue first; тоже самое
+			console.log(`Three level: ${k}`)
+		}
+	}
+}
