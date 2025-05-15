81890af136ba92a02282ce08eb0e9aad87661f95
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-15 23:33:57 +0300

    Задача 12

---
+++ Богдан - задачи/Задача 12.js
@@ -0,0 +1,10 @@
+// Создание объекта
+const person = {
+	name: 'Katrin',
+	surname: 'Viktorova',
+	favoriteNumber: 22,
+};
+
+const personInfo = `My name is ${person.name} ${person.surname} and my favorite numbers is ${person.favoriteNumber}`;
+
+console.log(personInfo);
