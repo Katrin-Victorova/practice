e4fb777aee6f8667fc0002592f35f208b75980ea
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-17 23:22:13 +0300

    Руслан 200 вопросов

---
+++ Собеседовании/Руслан 200 вопросов.js
@@ -0,0 +1,83 @@
+// // Сравнение переменных
+console.log(x);
+var x = 5;
+// // undefuned
+
+// // Что выведет?
+const calc = () => {
+	return 2 + 2;
+};
+// console.log(typeof null) // object
+// console.log(typeof calc) // function
+// console.log(calc instanceof Object); // true (функция — объект)
+
+let a = 1;
+// console.log(a++) // 1
+// console.log(++a) // 3
+// console.log(a) // 3
+
+let i = 0;
+
+while (i < 3) {
+	console.log(i);
+	i++;
+} // 0, 1, 2
+
+let arr7 = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr7.length; i++) {
+	if (arr7[i] === 3) {
+		break;
+	}
+	console.log(arr7[i]);
+} // 1, 2
+
+let iq = 0;
+while (true) {
+	iq++;
+	if (iq === 10) {
+		break;
+	}
+}
+
+console.log(iq); // 10
+
+let arr1 = [10, 20, 30];
+for (let i in arr1) {
+	console.log(i);
+} // ключи переберает индекст в массиве 0 1 2
+for (let value of arr1) {
+	console.log(value);
+} // значения перебираем 10 20 30
+
+const sentence = 'Hello, world! This is a sentence.';
+const worlds = sentence.split(' ', 3);
+console.log(worlds); // мы разделяем на массив слов (пробелы)
+// [ 'Hello,', 'world!', 'This' ]
+
+// Сначала преобразует значение в число, затем проверяет, является ли результат NaN.
+console.log(isNaN(NaN)); // true
+console.log(isNaN(123)); // false
+console.log(isNaN('123')); // false
+console.log(isNaN('abc')); // true
+
+// Не преобразует значение.
+// Проверяет, является ли переданный аргумент точно NaN и имеет тип number.
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.isNaN(123)); // false
+console.log(Number.isNaN('123')); // false
+console.log(Number.isNaN('abc')); // false строка (string), а не number.
+
+let user = { name: 'John', age: 30 };
+let clone = user; // поверхностное копирование
+clone.name = 'Pete';
+console.log(user.name); // измениться по ссылке Pete
+
+let obj = {
+	name: 'Alice',
+	sayHi() {
+		console.log(this.name);
+	},
+};
+
+let fn = obj.sayHi; // скопировали функциию без контекста
+fn(); // undefined
