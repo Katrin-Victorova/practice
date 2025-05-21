78c51e993c64d0f67bf61c61fb45f9bcac409a0a
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-21 09:50:32 +0300

    ОНС_??

--- Логические операторы/ОНС_??.js
+++ Логические операторы/ОНС_??.js
@@ -1,30 +1,36 @@
 /**
- * Оператор нулевого слияния (??) был введён в ECMAScript 2020 (ES11). 
- * Он используется для возврата первого не null и не undefined значения. 
- * Это удобно, когда нужно задать значение по умолчанию, если 
+ * Оператор нулевого слияния (??) был введён в ECMAScript 2020 (ES11).
+ * Он используется для возврата первого не null и не undefined значения.
+ * Это удобно, когда нужно задать значение по умолчанию, если
  * переменная не определена или равна null.
  */
 
+let score = 0;
+let defaultScore = 10;
+console.log(score || defaultScore); // 10 (0 считается ложным)
+console.log(score ?? defaultScore); // 0 (0 не является null или undefined)
+
 let username = null;
-let defaultUsername = "Гость";
+let defaultUsername = 'Гость';
 
 let currentUser = username ?? defaultUsername;
-console.log(currentUser);  // "Гость"
+console.log(currentUser); // "Гость"
 
 // Значения по умолчанию для нескольких переменных
 let firstName = null;
-let lastName = "Doe";
+let lastName = 'Doe';
 
-let displayName = (firstName ?? "Имя не указано") + " " + (lastName ?? "Фамилия не указана");
-console.log(displayName);  // "Имя не указано Doe"
-// Оператор ​​​​??​​​​ позволяет задать значения по умолчанию для каждой переменной отдельно. 
+let displayName =
+	(firstName ?? 'Имя не указано') + ' ' + (lastName ?? 'Фамилия не указана');
+console.log(displayName); // "Имя не указано Doe"
+// Оператор ​​​​??​​​​ позволяет задать значения по умолчанию для каждой переменной отдельно.
 // В этом примере, если ​​​​firstName​​​​ или ​​​​lastName​​​​ равны ​​​​null​​​​ или ​​​​undefined​​​​, используются значения по умолчанию.
 
 // Использование в функциях с параметрами
 function greet(name) {
-  let userName = name ?? "Гость";
-  console.log("Привет, " + userName);
+	let userName = name ?? 'Гость';
+	console.log('Привет, ' + userName);
 }
 
-greet();  // Привет, Гость
-greet("Алексей");  // Привет, Алексей
+greet(); // Привет, Гость
+greet('Алексей'); // Привет, Алексей
