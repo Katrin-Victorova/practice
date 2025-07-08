b25af0f2e7b2c30d258c9c094782b4141d2857df
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-07-08 13:18:08 +0300

    был дубль страницы

--- Методы Массивов/Логика методов.js
+++ Методы Массивов/Логика методов.js
@@ -1,4 +1,4 @@
-// 3 июля + лайфкодинг
+// 3 июля
 
 const nums = [1, 2, 3, 4];
 const fruits = ['apple', 'banana'];
@@ -15,6 +15,7 @@ function logFruit(fruit) {
 	console.log(fruit);
 }
 
+// 2 июля + лайфкодинг | футбол
 const players = [
 	{ id: 1, name: 'Лионель Месси', teamId: 1, countryId: 1, goals: 30 },
 	{ id: 2, name: 'Криштиану Роналду', teamId: 2, countryId: 2, goals: 25 },
@@ -74,3 +75,73 @@ const slicOfPlayers = players.slice(3);
 const slicOfPlayers1 = players.slice(0, 3);
 const changedListOfPlayers = slicOfPlayers.concat(slicOfPlayers1);
 // console.log(changedListOfPlayers)
+
+// 3 июня + лайфкодинг
+const transactions = [
+	{ userID: 'user1', amount: 200, date: '2023-01-01' },
+	{ userID: 'user2', amount: 500, date: '2023-01-02' },
+	{ userID: 'user1', amount: 300, date: '2023-01-03' },
+	{ userID: 'user3', amount: 400, date: '2023-01-01' },
+	{ userID: 'user2', amount: 150, date: '2023-01-04' },
+	{ userID: 'user3', amount: 250, date: '2023-01-02' },
+	{ userID: 'user4', amount: 100, date: '2023-01-01' },
+];
+
+// напишем функцию которая принимает id user и возвращает сумму всех его транзакций
+const userID = 'user1';
+// transactions.filter(item => item.userID === userID)
+const totalAmount = transactions.reduce((acc, transaction) => {
+	if (transaction.userID === userID) {
+		return acc + transaction.amount;
+	}
+	return acc;
+}, 0);
+console.log(totalAmount);
+
+// вернуть массив массивов где 1 - user, а 2 - это его дата
+const array = transactions.map(transactions => [
+	transactions.userID,
+	transactions.date,
+]);
+console.log(array);
+
+// группировка суммы, транзакций по userId
+function sortTransactions(arr) {
+	const userTransaction = {};
+
+	for (let transaction of arr) {
+		const { userID, amount } = transaction;
+
+		if (!userTransaction[userID]) {
+			userTransaction[userID] = 0;
+		}
+		userTransaction[userID] += amount;
+	}
+	return userTransaction;
+}
+
+console.log(sortTransactions(transactions));
+
+// отсортировать транзакции - Сначала больше потом маленькие
+function sortTransaction(transactions) {
+	const sorted = [...transactions].sort((a, b) => b.amount - a.amount);
+	return sorted;
+}
+
+console.log(sortTransaction(transactions));
+
+// сгруппировать по дате транзакции, выведешь топ дней по сумме транзакций
+const toDaysByTransactions = transactions => {
+	const top = {};
+	transactions.forEach(transaction => {
+		if (top[transaction.date]) {
+			top[transaction.date] = top[transaction.date].concat([transaction]);
+		} else {
+			top[transaction.date] = [transaction];
+		}
+	});
+
+	return top;
+};
+
+console.log(toDaysByTransactions(transactions));
